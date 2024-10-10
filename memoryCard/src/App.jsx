b1df12e82d67192a84cf641ec42f99db009d88d0
canofbubly <derekrussellbroden@gmail.com>
2024-10-09 19:13:36 -0500

    react setup

---
+++ memoryCard/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  //API IMAGE TEST
+  let testImg = 'replace me with url'
+  //PAGE GO HERE:
+  return(
+    <div className="page">
+      <div className="image1">{testImg}</div>
+    </div>
+  )
+}
+
+export default App
